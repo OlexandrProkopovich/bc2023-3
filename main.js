82fb055e897ad44b4fbd770a756eb9f85a39b9b8
OlexandrProkopovich <oleksandr.prokopovych@Inu.edu.ua>
2023-10-07 01:50:27 +0300

    Test

---
+++ main.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function ReadFile (fileName)
+{
+    const data = fs.readFileSync(fileName, 'utf-8');
+    return JSON.parse(data);
+}
+
+function WriteFile (fileName, result)
+{
+    const ReaderFile = result.map(result => `${result.category}: ${result.value}`).join('\n');
+    fs.writeFileSync(fileName, ReaderFile);
+}
+
+function AnalyzeJSON (jsonData)
+{
+    result = [];
+
+    for (item of jsonData)
+    {
+        if (item.txt === "Доходи, усього" || item.txt === "Витрати, усього")
+        {
+            result.push({category: item.txt, value: item.value});
+        }
+    }
+    return result;
+}
+
+function main()
+{
+    fileInput = "/Users/oleksandrprokopovic/Desktop/WEB_LAB3/data.json";
+    fileOutPut = "/Users/oleksandrprokopovic/Desktop/WEB_LAB3/output.txt";
+
+    try{
+        const dataJSON = ReadFile(fileInput);
+        const Analyze = AnalyzeJSON(dataJSON);
+        WriteFile(fileOutPut, result);
+        console.log("ваш вайл записано у файл");
+    } catch(error)
+    {
+        console.log("ПОМИЛКА(((((");
+    }
+}
+main();
